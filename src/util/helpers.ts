83912a6743478c8cf050a59448c514a80b05a6ae
Dimerson Daniel <dimerson.daniel@gmail.com>
2022-03-17 13:32:37 -0300

    remove replace of helpers

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -13,7 +13,7 @@ export const Json2Csv = (data: Array<any>): string => {
     const header = Object.keys(data[0]);
     const csv = [
         header.join(','),
-        ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer).replace(/"/gmi,"")).join(','))
+        ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
     ].join('\r\n')
 
     return csv;
